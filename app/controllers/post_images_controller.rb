6bd9ccf4891affab06ddc2776da46f5c3b2cd715
gmgn120 <gmgn120@gmail.com>
2021-03-07 06:39:54 +0000

    Add post_images/create

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,6 +4,10 @@ class PostImagesController < ApplicationController
   end
 
   def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -14,4 +18,11 @@ class PostImagesController < ApplicationController
 
   def destroy
   end
+  
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
 end
