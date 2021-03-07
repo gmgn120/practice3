c2a12063465e48d3b424412a569495b1bec27fe6
gmgn120 <gmgn120@gmail.com>
2021-03-07 06:46:52 +0000

    Add post_images/index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '56cfb2840aa9b9e3c51e0e0216119dbb4305835d76397d43c7fe1b3f1dc8f407b871993e88be9e9c6d012bb2111c8cf3c8803d6b91d1c749c00d0bc3d3acaa10'
