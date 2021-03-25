abb4d37f6f4d58fc0fe3c0637149580b3cf8804c
gmgn120 <gmgn120@gmail.com>
2021-03-25 08:07:38 +0000

    Add post_images/success message

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,6 +7,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
+    flash[:success] = "successfully"
     redirect_to post_images_path
     else
       render :new
